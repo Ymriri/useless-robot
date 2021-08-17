6cd46d34a60d6b07a4f19ed1fb290136909177cd
wuyou <1097810498@qq.com>
2021-08-17 17:39:25 +0800

    如果私聊消息的字段值包含groupCode和groupName会被替换为空

--- src/main/java/pers/wuyou/robot/config/MessageReplaceConfig.java
+++ src/main/java/pers/wuyou/robot/config/MessageReplaceConfig.java
@@ -2,30 +2,37 @@ package pers.wuyou.robot.config;
 
 import love.forte.simbot.api.message.events.GroupMsg;
 import love.forte.simbot.api.message.events.MsgGet;
+import love.forte.simbot.api.message.events.PrivateMsg;
 import pers.wuyou.robot.common.Cat;
 
 /**
  * @author wuyou
  */
 public class MessageReplaceConfig {
-    private MessageReplaceConfig(){}
+    private MessageReplaceConfig() {
+    }
 
     @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
     public static String reconstructMessage(MsgGet msg, String message) {
+        String atBot = "\\$\\{@bot}";
+        String atQQ = "\\$\\{@sender}";
+        String accountCodeRegex = "\\$\\{accountCode}";
+        String accountNameRegex = "\\$\\{accountName}";
+        String groupCodeRegex = "\\$\\{groupCode}";
+        String groupNameRegex = "\\$\\{groupName}";
         if (msg instanceof GroupMsg) {
-            String groupCodeRegex = "\\$\\{groupCode}";
-            String groupNameRegex = "\\$\\{groupName}";
             String groupCode = ((GroupMsg) msg).getGroupInfo().getGroupCode();
             String groupName = ((GroupMsg) msg).getGroupInfo().getGroupName();
             message = message
                     .replaceAll(groupCodeRegex, groupCode)
                     .replaceAll(groupNameRegex, groupName == null ? groupCode : groupName)
             ;
+        } else if (msg instanceof PrivateMsg) {
+            message = message
+                    .replaceAll(groupCodeRegex, "")
+                    .replaceAll(groupNameRegex, "")
+            ;
         }
-        String atBot = "\\$\\{@bot}";
-        String atQQ = "\\$\\{@sender}";
-        String accountCodeRegex = "\\$\\{accountCode}";
-        String accountNameRegex = "\\$\\{accountName}";
         String accountCode = msg.getAccountInfo().getAccountCode();
         String accountName = msg.getAccountInfo().getAccountNickname();
         return message
