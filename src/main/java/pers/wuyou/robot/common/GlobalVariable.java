3b953971f8869f24d93e12d9e0a335806fd4744d
wuyou <1097810498@qq.com>
2021-09-02 11:28:16 +0800

    添加 获取群成员缓存方法 添加 全局项目路径变量

--- src/main/java/pers/wuyou/robot/common/GlobalVariable.java
+++ src/main/java/pers/wuyou/robot/common/GlobalVariable.java
@@ -1,6 +1,8 @@
 package pers.wuyou.robot.common;
 
 import lombok.Getter;
+import love.forte.simbot.api.message.results.GroupMemberInfo;
+import love.forte.simbot.api.message.results.SimpleGroupInfo;
 import love.forte.simbot.api.sender.BotSender;
 import love.forte.simbot.bot.BotManager;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,7 +21,13 @@ import java.util.concurrent.TimeUnit;
  *
  * @author wuyou
  */
+@SuppressWarnings("unused")
 public class GlobalVariable {
+    /**
+     * 项目路径
+     */
+    @Getter
+    public static final String PROJECT_PATH;
     /**
      * 线程池
      */
@@ -29,6 +37,10 @@ public class GlobalVariable {
      */
     @Getter
     private static final Map<String, Boolean> BOOT_MAP;
+    /**
+     * 群成员缓存
+     */
+    private static final Map<String, AccountInfo> MEMBER_INDEX;
     /**
      * 机器人管理员
      */
@@ -51,7 +63,9 @@ public class GlobalVariable {
     private static BotSender sender;
 
     static {
+        PROJECT_PATH = System.getProperty("user.dir");
         BOOT_MAP = new HashMap<>();
+        MEMBER_INDEX = new HashMap<>();
         ADMINISTRATOR = new ArrayList<>();
         THREAD_POOL = new ThreadPoolExecutor(50, 50, 200, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), r -> {
             Thread thread = new Thread(r);
@@ -67,6 +81,48 @@ public class GlobalVariable {
         GlobalVariable.applicationContext = applicationContext;
         GlobalVariable.botManager = applicationContext.getBean(BotManager.class);
         GlobalVariable.sender = GlobalVariable.botManager.getDefaultBot().getSender();
+        GlobalVariable.THREAD_POOL.execute(() -> {
+            love.forte.simbot.api.sender.Getter getter = GlobalVariable.getter();
+            for (SimpleGroupInfo groupInfo : getter.getGroupList()) {
+                for (GroupMemberInfo memberInfo : getter.getGroupMemberList(groupInfo.getGroupCode())) {
+                    GlobalVariable.addMemberIndex(memberInfo.getAccountCode(), groupInfo.getGroupCode());
+                }
+            }
+        });
+    }
+
+    public static synchronized void addMemberIndex(String code, String group) {
+        if (GlobalVariable.MEMBER_INDEX.get(code) == null) {
+            GlobalVariable.MEMBER_INDEX.put(code, new AccountInfo(code, group));
+        } else {
+            GlobalVariable.MEMBER_INDEX.get(code).addGroup(group);
+        }
+    }
+
+    public static synchronized void removeMemberIndex(String code, String group) {
+        AccountInfo accountInfo = GlobalVariable.MEMBER_INDEX.get(code);
+        if (accountInfo != null) {
+            accountInfo.getGroupList().remove(group);
+            if (accountInfo.getGroupList().size() == 0) {
+                GlobalVariable.MEMBER_INDEX.remove(code);
+            }
+        }
+    }
+
+    public static synchronized AccountInfo getAccountFromMemberIndex(String code) {
+        return GlobalVariable.MEMBER_INDEX.get(code);
+    }
+
+    public static love.forte.simbot.api.sender.Getter getter() {
+        return sender.GETTER;
+    }
+
+    public static love.forte.simbot.api.sender.Sender sender() {
+        return sender.SENDER;
+    }
+
+    public static love.forte.simbot.api.sender.Setter setter() {
+        return sender.SETTER;
     }
 
     public static String getDefaultBotCode() {
